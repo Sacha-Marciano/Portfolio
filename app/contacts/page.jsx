2f547230556d81ccd152e6c2deaa443653569c12
Sacha-Marciano <Momarciano26@gmail.com>
2024-12-18 12:38:43 +0200

    Implement projects page

--- app/contacts/page.jsx
+++ app/contacts/page.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Select,
+  SelectGroup,
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+import { motion } from "framer-motion";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+972) 53-708-0183",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "momarciano26@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Adress",
+    description: "Ra'anana, Israel",
+  },
+];
 
 const Contacts = () => {
-  return <div>Contacts</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* forms */}
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent"> Let's work together !</h3>
+              <p className="text-white/60">
+                wkrnv;ewnvpenvqoeanv;ekqng;a emvqepojv'ae ernegnieop wjrwpw
+                wwipfw ecrjnvje
+              </p>
+              {/* inputs */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="Firstname" />
+                <Input type="lastname" placeholder="Lastname" />
+                <Input type="email" placeholder="Email adress" />
+                <Input type="phone" placeholder="Phone number" />
+              </div>
+            </form>
+          </div>
+          {/* info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            info
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Contacts;
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -15,21 +15,171 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
-const projects = [
-  {
-    num: "01",
-    category: "frontend",
-    title: "project 1",
-    description:
-      "mesofne flknvelnr aebciwbf sfjvneornewo sdkc skbqne sf vks caienfow cewjf jdsjacnonc ",
-  },
-];
-
 import Link from "next/link";
 import Image from "next/image";
 
+//components
+import ProjectSliderButtons from "@/components/ProjectSliderButtons";
+
+const projects = [
+  {
+    num: "01",
+    category: "Full Stack",
+    title: "Helpeat",
+    description:
+      "mesofne flknvelnr aebciwbf sfjvneornewo sdkc skbqne sf vks caienfow cewjf jdsjacnonc ",
+    stack: [
+      { name: "React" },
+      { name: "MongoDB" },
+      { name: "Express" },
+      { name: "Node.js" },
+      { name: "Javascript" },
+    ],
+    image: "/assets/projects/Helpeat/Helpeat.png",
+    live: "https://helpeat.jumpingcrab.com",
+    github: "https://github.com/Sacha-Marciano/HelpEat_frontend",
+  },
+  {
+    num: "02",
+    category: "frontend",
+    title: "Coffeeshop",
+    description:
+      "mesofne flknvelnr aebciwbf sfjvneornewo sdkc skbqne sf vks caienfow cewjf jdsjacnonc ",
+    stack: [{ name: "HTML 5" }, { name: "CSS 3" }, { name: "Javascript" }],
+    image: "/assets/projects/Coffeeshop/Coffeeshop.png",
+    live: "",
+    github: "https://github.com/Sacha-Marciano/se_project_coffeeshop",
+  },
+  {
+    num: "03",
+    category: "frontend",
+    title: "Portfolio",
+    description:
+      "mesofne flknvelnr aebciwbf sfjvneornewo sdkc skbqne sf vks caienfow cewjf jdsjacnonc ",
+    stack: [
+      { name: "Next.js" },
+      { name: "Tailwind.css" },
+      { name: "Javascript" },
+    ],
+    image: "/assets/Portfolio/Portfolio.png",
+    live: "/",
+    github: "",
+  },
+];
+
 const Projects = () => {
-  return <div>projects page</div>;
+  const [project, setProject] = useState(projects[0]);
+
+  const handleSlideChange = (swiper) => {
+    const currentIndex = swiper.activeIndex;
+    setProject(projects[currentIndex]);
+  };
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          {/* project info */}
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="flex flex-col gap-[30px]">
+              {/* outline num */}
+              <div className="text-8xl leading-none font-extrabold">
+                {project.num}
+              </div>
+              {/* project category */}
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
+              {/* project description */}
+              <p className="text-white/60">{project.description} </p>
+              {/* stack */}
+              <ul className="flex gap-4 flex-wrap">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accent">
+                      {item.name}
+                      {index !== project.stack.length - 1 && ","}
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="border border-white/20"></div>
+              {/* buttons */}
+              <div className="flex items-center gap-4">
+                {/* live project */}
+                <Link href={project.live} target="_blank">
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live Project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                {/* github project */}
+                <Link href={project.github} target="_blank">
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+          </div>
+          {/* slider */}
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((item, index) => {
+                return (
+                  <SwiperSlide key={index} className="w-full">
+                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                      {/* overlay */}
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                      {/* image */}
+                      <div className="relative w-full h-full">
+                        <Image
+                          src={project.image}
+                          fill
+                          className="object-contain"
+                          alt="project image"
+                        />
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+              {/* slider buttons */}
+              <ProjectSliderButtons
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+              />
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Projects;
