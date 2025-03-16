e97e81ac3fb93ea6f2856ec76cb635c825e231e0
Sacha-Marciano <Momarciano26@gmail.com>
2025-03-16 10:46:31 +0200

    fix main screen display

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ import Stats from "@/components/Stats";
 
 const Home = () => {
   return (
-    <section className="h-[90vh]">
+    <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* texts */}
